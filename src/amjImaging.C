ad8dfd9e94700699a6db416623a172a43d03deed
Anders M. Jorgensen <andersmjorgensen@gmail.com>
2024-12-12 17:17:34 -0700

    Various changes which were made in June 2024.

--- src/amjImaging.C
+++ src/amjImaging.C
@@ -2,7 +2,7 @@
 
 namespace amjImaging{
 
-  int fwrite(FILE *fp, const amjLos &los){
+  int fwrite(FILE *fp, const Los &los){
     int n=fwrite(fp,los.p());
     n+=fwrite(fp,los.d());
     return n;
@@ -10,32 +10,30 @@ namespace amjImaging{
   
 
   template<>
-  int fwrite(FILE *fp, const amjImage<amjLos> &image){
+  int fwrite(FILE *fp, const Image<Los> &image){
     unsigned int nR,nC;
     image.size(nR,nC);
     int n=fwrite(&nR,sizeof(int),1,fp);
     n+=fwrite(&nC,sizeof(int),1,fp);
-    for(unsigned int iR=0;iR<nR;iR++)
-      for(unsigned int iC=0;iC<nC;iC++)
-	n+=fwrite(fp,image[iR][iC]);
+    for(unsigned int i=0;i<nR*nC;i++)
+      n+=fwrite(fp,image[i]);
     return n;
   }
 
-  int fread(FILE *fp, amjLos &los){
+  int fread(FILE *fp, Los &los){
     int n=fread(fp,los.p());
     n+=fread(fp,los.d());
     return n;
   }
   
   template<>
-  int fread(FILE *fp, amjImage<amjLos> &image){
+  int fread(FILE *fp, Image<Los> &image){
     int nR,nC;
     int n=fread(&nR,sizeof(int),1,fp);
     n+=fread(&nC,sizeof(int),1,fp);
     image.resize(nR,nC);
-    for(int iR=0;iR<nR;iR++)
-      for(int iC=0;iC<nC;iC++)
-	n+=fread(fp,image[iR][iC]);
+    for(unsigned int i=0;i<image.size();i++)
+	n+=fread(fp,image[i]);
     return n;
   }
   
